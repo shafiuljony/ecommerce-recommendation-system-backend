042cddffef158d3f08b55b5d58e819e0e862041f
shafiuljony <shafiuljony12@gmail.com>
2023-01-23 12:03:54 +0600

    sidebar issus

--- public/admin/js/custom.js
+++ public/admin/js/custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $(".nav-item").removeClass("active");
+    $(".nav-link").removeClass("active");
+
     // Checking admin password is correct or not 
     $("#current_password").keyup(function(){
         var current_password = $("#current_password").val();
