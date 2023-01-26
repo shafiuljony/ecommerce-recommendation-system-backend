1c425604db745103aa6fae1c11e084bf9084ab53
shafiuljony <shafiuljony12@gmail.com>
2023-01-27 01:22:43 +0600

    categories issue fixed

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Session;
 
 class CategoryController extends Controller
 {
     public function categories(){
+        Session::put('page','categories');
         $categories = Category::get()->toArray();
         // dd($categories);
         return view('admin.categories.categories')->with(compact('categories'));
