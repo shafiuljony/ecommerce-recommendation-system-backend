1f169d455a28da854ba95cfc985418dfef23976f
shafiuljony <shafiuljony12@gmail.com>
2023-01-27 00:55:57 +0600

    created catagory table data seed

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categoryRecords = [
+            ['id'=>1,'parent_id'=>0,'section_id'=>1,'category_name'=>'Desktop','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'computer','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1],
+            ['id'=>2,'parent_id'=>0,'section_id'=>1,'category_name'=>'Laptop','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'laptop','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1],
+            ['id'=>3,'parent_id'=>0,'section_id'=>1,'category_name'=>'Mobile','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'mobile','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1]
+        ];
+        Category::insert($categoryRecords);
+    }
+}
