dc8d1ae4c6fad5af4be3bee0e7921b1597dcbf88
shafiuljony <shafiuljony12@gmail.com>
2023-01-27 12:26:42 +0600

    category added with updating the status

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -11,8 +11,22 @@ class CategoryController extends Controller
 {
     public function categories(){
         Session::put('page','categories');
-        $categories = Category::get()->toArray();
+        $categories = Category::with(['section','parentcategory'])->get()->toArray();
         // dd($categories);
         return view('admin.categories.categories')->with(compact('categories'));
+    }
+
+    public function updateCategoryStatus(Request $request){
+        if($request->ajax()){
+            $data = $request->all();
+            // echo "<pre>"; print_r($data); die;
+            if($data['status']=="Active"){
+                $status = 0;
+            }else{
+                $status = 1;
+            }
+            Category::where('id',$data['category_id'])->update(['status'=>$status]);
+            return response()->json(['status'=>$status,'category_id'=>$data['category_id']]);
+        }
     }
 }
