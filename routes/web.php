d007786798756fb5b12a46d8cf68e034a47a57b3
shafiuljony <shafiuljony12@gmail.com>
2023-01-29 17:57:06 +0600

    catagory subcategory

--- routes/web.php
+++ routes/web.php
@@ -90,6 +90,8 @@ Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function
 
         Route::match(['get','post'],'add-edit-category/{id?}','CategoryController@addEditCategory');
 
+        Route::get('append-categories-level','CategoryController@appendCategoryLevel');
+
     });   
 });
 
