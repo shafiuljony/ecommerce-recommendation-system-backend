18acef390c1597e4a28763226ca894ce01c1ca1b
shafiuljony <shafiuljony12@gmail.com>
2023-01-30 14:36:34 +0600

    created brand table seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(VendorsBusinessDetailsTableSeeder::class);
         // $this->call(VendorsBankDetailsTableSeeder::class);
         // $this->call(SectionsTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
+        // $this->call(CategoryTableSeeder::class);
+        $this->call(BrandsTableSeeder::class);
     }
 }
