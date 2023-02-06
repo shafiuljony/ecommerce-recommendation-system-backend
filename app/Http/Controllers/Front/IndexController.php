b30f6d8a7b472f11dd032bf7a59cc306cb520de9
shafiuljony <shafiuljony12@gmail.com>
2023-02-06 21:33:04 +0600

    new products showing

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -11,6 +12,8 @@ class IndexController extends Controller
     public function index(){
         $sliderBanners =  Banner::where('type','Slider')->where('status',1)->get()->toArray();
         $fixBanners =  Banner::where('type','Fix')->where('status',1)->get()->toArray();
+        $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(8)->get()->toArray();
+        dd($newProducts);
         return view('front.index')->with(compact('sliderBanners','fixBanners'));
     }
 }
