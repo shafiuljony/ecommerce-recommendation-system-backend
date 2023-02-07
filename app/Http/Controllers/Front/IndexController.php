abf79c6d5fcaa6dfcbde4108ab8d1ef47dabdd00
shafiuljony <shafiuljony12@gmail.com>
2023-02-07 14:41:32 +0600

    product updated

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -14,7 +14,9 @@ class IndexController extends Controller
         $fixBanners =  Banner::where('type','Fix')->where('status',1)->get()->toArray();
         $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(8)->get()->toArray();
         $bestSellers = Product::where(['is_bestseller'=>'Yes','status'=>1])->inRandomOrder()->get()->toArray();
-        // dd($bestSellers);
-        return view('front.index')->with(compact('sliderBanners','fixBanners','newProducts','bestSellers'));
+        $discounterProducts = Product::where('product_discount','>',0)->where('status',1)->limit(6)->inRandomOrder()->get()->toArray();
+        $isfeatured = Product::where(['is_featured'=>'Yes','status'=>1])->inRandomOrder()->get()->toArray();
+        // dd($discounterProducts);
+        return view('front.index')->with(compact('sliderBanners','fixBanners','newProducts','bestSellers','discounterProducts','isfeatured'));
     }
 }
