0dcdcd6bee753aa11603a2b1c070e9ea8e105fda
shafiuljony <shafiuljony12@gmail.com>
2023-02-07 17:14:42 +0600

    fathing ids

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +11,16 @@ class ProductsController extends Controller
 {
     public function listing(){
         $url = Route::getFacadeRoot()->current()->uri();
+        $categoryCount = Category::where(['url'=>$url,'status'=>1])->count();
+
+        if($categoryCount >0){
+            //Get Category Details
+            $categoryDetails = Category::categoryDetails($url);
+            dd($categoryDetails);
+            echo "category existis"; die;
+
+        }else{
+            abort(404);
+        }
     }
 }
