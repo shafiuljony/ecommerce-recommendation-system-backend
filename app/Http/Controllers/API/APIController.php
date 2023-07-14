88f7a55c7de9111cdf50a627ac5d8d149d795811
shafiuljony <shafiuljony12@gmail.com>
2023-07-14 12:25:27 +0600

    user email via reactjs app

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Validator;
 use Illuminate\Http\Request;
 
 class APIController extends Controller
@@ -12,6 +13,31 @@ class APIController extends Controller
         if($request->isMethod("post")){
             $data = $request->input();
             // echo "<pre/>"; print_r($data); die;
+
+            $rules = [
+                'name' => 'required',
+                'mobile' => 'required|numeric',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6|max:20',
+            ];
+
+            $customMessages = [
+                'name.required' => 'Name is required',
+                'mobile.required' => 'Mobile is required',
+                'mobile.numeric' => 'Mobile must be numeric',
+                'email.required' => 'Email is required',
+                'email.email' => 'Email must be valid',
+                'email.unique' => 'Email already exists',
+                'password.required' => 'Password is required',
+                'password.min' => 'Password must be atleast 6 characters',
+                'password.max' => 'Password must be less than 20 characters',
+            ];
+
+            $validator = Validator::make($data,$rules,$customMessages);
+
+            if($validator->fails()){
+                return response()->json($validator->errors(),433);
+            }
 
             $user = new User;
             $user->name = $data['name'];
@@ -22,6 +48,35 @@ class APIController extends Controller
             $user->save();
 
             return response()->json(['status'=>true,'message'=>'User Registered Successfully!'],201);
+        }
+    }
+
+    public function loginUser(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->input();
+
+            // echo "<pre/>"; print_r($data); die;
+
+            //Verify User Email
+            $userCount = User::where('email',$data['email'])->first();
+            if($userCount > 0){
+                 //Featch User Details
+                $userDetails = User::where('email',$data['email'])->first();
+
+                //Verify Password
+                if(password_verify($data['password'],$userDetails->password)){
+                    return response()->json(['status'=>true,
+                    "message"=>"User Logged In Successfully!"],201);
+                }else{
+                    $message = "Password is Incorrect!";
+                    return response()->json(['status'=>false,'message'=>$message],422);
+                }
+            }else{
+                $message = "Email is Incorrect!";
+                return response()->json(['status'=>false,'message'=>$message],422);
+            }
+
+           
         }
     }
 }
